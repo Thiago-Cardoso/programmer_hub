0401f952ee5dff6264599c62ce2705b6ed2afd56
vagrant <vagrant@vagrant.vm>
2017-12-05 03:41:56 +0000

    estrutura e login com devise

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :authenticate_user! #verifica se esta logado
+
+protected
+
+  def after_sign_in_path_for(current_user)
+    posts_path
+  end
+
+  def after_sign_up_path_for(current_user)
+    posts_path
+  end
+
+end
